eb5b231f3483a4929221cbc66400f281dcd3813b
ns <niksaysit@gmail.com>
2024-10-10 23:56:46 +0300

    Parse title and price of the product

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 import requests
 
 
@@ -8,7 +9,22 @@ def main():
     if res.status_code != 200:
         raise Exception(f"The request failed with code {res.status_code}")
 
-    print(res.text)
+    s = BeautifulSoup(res.text, "html.parser")
+    prods = s.find(class_="products-grid").find_all(class_="item-info")
+    for prod in prods:
+        print("\n", 20 * "=", "\n")
+
+        title = prod.find(class_="item-title").find("a")
+        print(f"Title: {title.text}")
+
+        price_span = (prod.find(class_="item-price")
+                      .find(class_="special-price"))
+        if price_span is None:
+            print("Price: None")
+            continue
+
+        price = price_span.find(class_="price")
+        print(f"Price: {price.text}")
 
 
 if __name__ == "__main__":
