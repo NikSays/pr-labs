8850ed73c1baff057e4731911f2a06677ee2d6d8
ns <niksaysit@gmail.com>
2024-10-18 01:22:52 +0300

    Low level HTTPS connection

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
 from bs4 import BeautifulSoup
-import requests
 from functools import reduce
 from datetime import datetime, timezone
+import ssl
+import json
+import socket
+
+
+def request(url):
+    host, path = url.replace("https://", "").split("/", 1)
+    port = 443
+
+    context = ssl.create_default_context()
+
+    sock = socket.create_connection((host, port))
+    wrapped_socket = context.wrap_socket(sock, server_hostname=host)
+
+    http_request = f"""GET /{path} HTTP/1.0\r
+Host: {host}\r
+Connection: close\r\n\r\n"""
+
+    wrapped_socket.sendall(http_request.encode())
+
+    response = b""
+    while True:
+        part = wrapped_socket.recv(4096)
+        if not part:
+            break
+        response += part
+
+    wrapped_socket.close()
+
+    response_str = response.decode()
+    headers, html_content = response_str.split("\r\n\r\n", 1)
+    return html_content
 
 
 def process_product(prod):
@@ -26,11 +57,9 @@ def process_product(prod):
             raise Exception(f'Price is not a number for product "{
                 title}"')
 
-    res = requests.get(link)
-    if res.status_code != 200:
-        raise Exception("Error getting product information")
+    res = request(link)
 
-    prod_page = BeautifulSoup(res.text, "html.parser")
+    prod_page = BeautifulSoup(res, "html.parser")
     warranty_str = prod_page.find(
         class_="product-warranty").find("span").text.replace(" months", "")
     try:
@@ -56,12 +85,9 @@ def add_eur(prod, rate):
 
 def main():
     url = "https://nanoteh.md/en/computers-servers-parts/monitors-displays-screens/"
-    res = requests.get(url)
+    res = request(url)
 
-    if res.status_code != 200:
-        raise Exception(f"The request failed with code {res.status_code}")
-
-    s = BeautifulSoup(res.text, "html.parser")
+    s = BeautifulSoup(res, "html.parser")
     prods = s.find(class_="products-grid").find_all(class_="item-info")
 
     product_info = []
@@ -72,7 +98,8 @@ def main():
         except Exception as e:
             print(f"Error: {e}. Skipping...")
 
-    # requests.get("https://open.er-api.com/v6/latest/MDL").json()["rates"]["EUR"]
+    # rate = json.loads(
+        # request("https://open.er-api.com/v6/latest/MDL"))["rates"]["EUR"]
     rate = 1/20
     product_info = filter(
         lambda p: p["price"] is not None and p["price"] < 2000,
