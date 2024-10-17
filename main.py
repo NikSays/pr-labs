47c4204f5071a66b57aa07bedcdb658380097110
ns <niksaysit@gmail.com>
2024-10-17 18:28:10 +0300

    Add data validation

--- main.py
+++ main.py
@@ -2,6 +2,48 @@ from bs4 import BeautifulSoup
 import requests
 
 
+def process_product(prod):
+    title_a = prod.find(class_="item-title").find("a")
+    link = title_a["href"]
+
+    title = title_a.text
+    if len(title) == 0:
+        raise Exception("Product with empty title")
+
+    price_span = (prod.find(class_="item-price")
+                  .find(class_="special-price"))
+
+    if price_span is None:
+        price = None
+    else:
+        price_str = price_span.find(class_="price").text.replace(
+            " ", "").replace("MDL", "")
+        try:
+            price = int(price_str)
+        except ValueError:
+            raise Exception(f'Price is not a number for product "{
+                title}"')
+
+    res = requests.get(link)
+    if res.status_code != 200:
+        raise Exception("Error getting product information")
+
+    prod_page = BeautifulSoup(res.text, "html.parser")
+    warranty_str = prod_page.find(
+        class_="product-warranty").find("span").text.replace(" months", "")
+    try:
+        warranty = int(warranty_str)
+    except ValueError:
+        raise Exception(f'Warranty duration is not a number for product "{
+            title}"')
+
+    return {
+        "title": title,
+        "price": price,
+        "warranty": warranty
+    }
+
+
 def main():
     url = "https://nanoteh.md/en/computers-servers-parts/monitors-displays-screens/"
     res = requests.get(url)
@@ -11,31 +53,23 @@ def main():
 
     s = BeautifulSoup(res.text, "html.parser")
     prods = s.find(class_="products-grid").find_all(class_="item-info")
+
+    product_info = []
+
     for prod in prods:
-        print("\n", 20 * "=", "\n")
+        try:
+            product_info.append(process_product(prod))
+        except Exception as e:
+            print(f"Error: {e}. Skipping...")
 
-        title = prod.find(class_="item-title").find("a")
-        print(f"Title: {title.text}")
+    for i in product_info:
+        print(20*"=", "\n")
 
-        link = title["href"]
+        print("Name: ", i["title"])
+        print("Price: ", i["price"])
+        print("Warranty: ", i["warranty"])
 
-        price_span = (prod.find(class_="item-price")
-                      .find(class_="special-price"))
-        if price_span is None:
-            print("Price: None")
-            continue
-
-        price = price_span.find(class_="price")
-        print(f"Price: {price.text}")
-
-        res = requests.get(link)
-        if res.status_code != 200:
-            print("Error getting product information")
-            continue
-
-        prod_page = BeautifulSoup(res.text, "html.parser")
-        warranty = prod_page.find(class_="product-warranty").find("span")
-        print(f"Warranty: {warranty.text}")
+        print("\n")
 
 
 if __name__ == "__main__":
